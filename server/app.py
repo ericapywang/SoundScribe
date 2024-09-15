0fefc7cb2ae28be7a8ed78e87fd9912a07978b63
ewang-21 <46633719+ewang-21@users.noreply.github.com>
2024-09-14 23:11:48 -0400

    updated flask code

--- server/app.py
+++ server/app.py
@@ -1,8 +1,10 @@
 # backend/app.py
 
 from flask import Flask, jsonify
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/api/data', methods=['GET'])
 def get_data():
--- server/record_audio.py
+++ server/record_audio.py
@@ -3,15 +3,21 @@ import numpy as np
 import scipy.io.wavfile as wavfile
 from pydub import AudioSegment
 from flask import Flask, jsonify
-
-app = Flask(__name__)
+from server import app
 
 # Function to record audio from the microphone
 @app.route('/api/record', methods=['GET'])
 def record_audio(duration=5, sample_rate=44100):
-    print("Recording for {} seconds...".format(duration))
+    print("Recording for {} seconds...".format(duration), file=sys.stderr)
     audio_data = sd.rec(int(duration * sample_rate), samplerate=sample_rate, channels=1, dtype='int16')
     sd.wait()  # Wait until the recording is finished
+
+    wav_filename = 'recorded_audio.wav'
+    save_wav(audio_data, sample_rate, wav_filename)
+
+    mp3_filename = 'recorded_audio.mp3'
+    convert_to_mp3(wav_filename, mp3_filename)
+
     return audio_data, sample_rate
 
 # Function to save the recorded audio as a WAV file
@@ -25,18 +31,15 @@ def convert_to_mp3(wav_filename, mp3_filename='output.mp3'):
     audio.export(mp3_filename, format="mp3")
     print("Converted to MP3: {}".format(mp3_filename))
 
-def main():
-    # Record audio from the microphone
-    duration = 5  # Seconds
-    audio_data, sample_rate = record_audio(duration)
+# def main():
+#     # Record audio from the microphone
+#     duration = 5  # Seconds
+#     audio_data, sample_rate = record_audio(duration)
 
-    # Save as WAV file
-    wav_filename = 'recorded_audio.wav'
-    save_wav(audio_data, sample_rate, wav_filename)
+#     # Save as WAV file
+#     wav_filename = 'recorded_audio.wav'
+#     save_wav(audio_data, sample_rate, wav_filename)
 
-    # Convert WAV to MP3
-    mp3_filename = 'recorded_audio.mp3'
-    convert_to_mp3(wav_filename, mp3_filename)
-
-if __name__ == '__main__':
-    main()
+#     # Convert WAV to MP3
+#     mp3_filename = 'recorded_audio.mp3'
+#     convert_to_mp3(wav_filename, mp3_filename)
