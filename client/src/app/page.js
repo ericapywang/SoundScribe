38af824066164be570951d25212fdf6d2c766e53
ewang-21 <46633719+ewang-21@users.noreply.github.com>
2024-09-14 21:10:07 -0400

    basic buttons for record/generate

--- client/src/app/page.js
+++ client/src/app/page.js
@@ -1,16 +1,26 @@
 // import Image from "next/image";
-import {Button} from '@mui/material/';
-import PlayArrowIcon from '@mui/icons-material/PlayArrow';
+import {Button, Box, Stack} from '@mui/material/';
+import MicIcon from '@mui/icons-material/Mic';
 
 export default function Home() {
   return (
     <div>
-      <Button>
-        <PlayArrowIcon></PlayArrowIcon>
-      </Button>
-      <Button>
-        Generate
-      </Button>
+      <Box
+        display='flex'
+        justifyContent='center'
+        alignItems='center'
+        height='90vh'
+        width='100vw'
+      >
+        <Stack spacing={5}>
+          <Button style={{backgroundColor: 'blue', borderRadius: '50%', width: 200, height: 200, minWidth: 0}}>
+            <MicIcon style={{color: 'white', width: 90, height: 90}}></MicIcon>
+          </Button>
+          <Button style={{backgroundColor: 'blue', width: 200, height: 60}}>
+            Generate
+          </Button>
+        </Stack>
+      </Box>
     </div>
   );
 }
